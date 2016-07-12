849f4d40fd28080102b8804992511d69ae2821f0
M3579 <mihirkasmalkar@gmail.com>
2016-07-12 12:51:47 -0700

    Fixed syntax error

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.post("/movement", function (req, res) {
 	res.send("");
 });
 
-app.listen((app.get("port"), function() {
+app.listen(app.get("port"), function() {
 	console.log("Server running on port 9000");
 })
